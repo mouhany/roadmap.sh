c3c471bc185c78757d9358b85fc1be07e003749c
Mouhany <mouhany@gmail.com>
2025-01-06 10:53:18 +0700

    toggle symbol in accordion-title

--- frontend/13-accordion/script.js
+++ frontend/13-accordion/script.js
@@ -14,6 +14,7 @@ document.addEventListener("DOMContentLoaded", () => {
         }
       });
       activeContent.classList.toggle("active-content");
+      activeTitle.classList.toggle("active-title");
     });
   });
 });
