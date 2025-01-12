7cf519b503505d3a8efe4c6fdf04d0f8654f67b6
Mouhany <mouhany@gmail.com>
2025-01-12 10:30:50 +0700

    remove flash cards source code from gh-pages branch

--- frontend/15-flash-cards/src/components/FlashCard.jsx
+++
@@ -1,9 +0,0 @@
-function FlashCard(props) {
-  return (
-    <div id="container-flashcard">
-      <h3>{props.isFlipped ? props.currentAnswer : props.currentQuestion}</h3>
-    </div>
-  );
-}
-
-export default FlashCard;
