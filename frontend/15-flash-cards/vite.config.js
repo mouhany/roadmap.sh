ff380856d05b3388a2b7c9c4e1835481231ded5c
Mouhany <mouhany@gmail.com>
2025-01-12 07:47:37 +0700

    update base url for flash cards

--- frontend/15-flash-cards/vite.config.js
+++ frontend/15-flash-cards/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/roadmap.sh/15-flash-cards/"
+  base: "/roadmap.sh/frontend/15-flash-cards/"
 })
