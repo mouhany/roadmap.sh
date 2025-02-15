ef4ac93b5fac23a4ad36f7633e94699e4616058b
Mouhany <mouhany@gmail.com>
2025-02-15 09:46:05 +0700

    update textarea counter

--- frontend/12-restricted-textarea/script.js
+++ frontend/12-restricted-textarea/script.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", () => {
   textareaInput.value =
     "You are required to create a textarea that tracks and displays the number of characters typed by the user, along with a maximum character limit. As the user types, the character count will update dynamically. Once the limit is reached, the textarea";
 
-  textareaCounter.textContent = "248 / 2";
+  textareaCounter.textContent = `${textareaInput.value.length} / ${maxLength - textareaInput.value.length}`;
 
   textareaInput.addEventListener("input", () => {
     const charLength = textareaInput.value.length;
